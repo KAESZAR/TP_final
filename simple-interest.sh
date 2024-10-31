b634bc2b551d75bdef762f3a73a414f5e1847113
CesDev <121060412+KAESZAR@users.noreply.github.com>
2024-10-31 10:52:39 -0300

    Update simple-interest.sh
    modificaciones a la calculadora de interes

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,19 +1,4 @@
-# TP FINALL
-# NO CHANGES !!
-# This script calculates simple interest given principal, annual rate of interest and time period in years.
-# Do not use this in production. Sample purpose only.
-
-# Author: Upkar Lidder (IBM)
-# Addtional Authors:
-# <KAESZAR>
-
-# Input:
-# a, principal amount
-# b, time period in years
-# c, annual rate of interest
-
-# Output:
-# simple interest = p*t*r
+A calculator that calculates simple inetrest given principal, annual rate of inetrest and time period in years
 
 echo "Enter the principal:"
 read a
